30a5af09be4ba9b033ed6d9b1f8ce6076c717f5d
keivik3 <vemedvedeva@edu.hse.ru>
2023-12-16 14:28:47 +0300

    docstring added

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,6 +7,15 @@ bot = telebot.TeleBot('6671933355:AAHDjP8ZIHh2e4sA7geU0tN4XUsEO-dLR_o')
 passwords = {}
 @bot.message_handler(commands=['start'])
 def start(message):
+    """function that generates password for the second player
+
+    :param message: message in telegram bot
+    :type message: dict
+    :return: sends message to player 2
+    :rtype: str | int
+    :raises NameError: if "random" or "string" module is not imported
+    """
+
     password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
 
     passwords[message.chat.id] = password
@@ -15,6 +24,15 @@ def start(message):
 
 @bot.message_handler(func=lambda message: True)
 def check_password(message):
+    """ checks whether the password is correct
+
+    :param message: message in telegram bot
+    :type message: dict
+    :return: confirms the password or don't
+    :rtype: str
+    :raises KeyError: if the message.chat.id is not found in the passwords dictionary
+    """
+
     if message.text == passwords.get(message.chat.id):
         bot.send_message(message.chat.id, "Вы успешно вошли в игру!")
     else:
